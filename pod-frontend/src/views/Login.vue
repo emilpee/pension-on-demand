8c0b109c2ec38f9a17072525ebb3b23f8d2d369e
Emil <emilpet93@gmail.com>
2019-11-12 15:31:20 +0100

    Created scss files and a login view

---
+++ pod-frontend/src/views/Login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="logotext">
+      <h1>Pension</h1>
+      <p>on demand</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss"> 
+
+</style>
